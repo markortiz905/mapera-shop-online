11cb2a096c3b84c0a630d2ebb50bb847f9fed89a
Mark Ortiz <ortizmark905@gmail.com>
2017-09-06 12:27:50 +0800

    initial commit with view jsp

---
+++ online-shop-mapera/src/main/java/com/mapera/shop/online/onlineshopmapera/OnlineShopMaperaApplication.java
@@ -0,0 +1,12 @@
+package com.mapera.shop.online.onlineshopmapera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopMaperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopMaperaApplication.class, args);
+	}
+}
---
+++ online-shop-mapera/src/test/java/com/mapera/shop/online/onlineshopmapera/OnlineShopMaperaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mapera.shop.online.onlineshopmapera;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineShopMaperaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
